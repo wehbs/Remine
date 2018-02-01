1a9f2b9ef927eea8ac60278071a837b60f822ac1
wehbs <shamirwehbe@me.com>
2018-02-01 02:07:48 -0500

    rendered api results to table

--- front-end-interview-test/src/CHANGEME.js
+++ front-end-interview-test/src/CHANGEME.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import RemineTable from './components/Table/RemineTable/RemineTable';
+import API from './API';
 
 class Test extends Component {
+
+state = {
+    property: []
+}
+
+componentDidMount() {
+    this.getLocations();
+  }
+
+    getLocations = () => {
+        API.getLocations()
+          .then(res =>
+            this.setState({
+              property: res.data
+            })
+
+          )
+          .catch(err => console.log(err));
+      };
+
     render() {
         return (
             <div className="testContainer">
                 <div className="filterContainer">
                     Your filters go here.
                 </div>
-                <RemineTable properties={[]} />
+                <RemineTable properties={this.state.property} />
             </div>
         );
     }
