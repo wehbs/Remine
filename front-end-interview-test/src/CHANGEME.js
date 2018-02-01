03fb9e529196a0bcc5c5fe99f7b6bea50a1bd07d
wehbs <shamirwehbe@me.com>
2018-02-01 13:57:58 -0500

    filtering is working

--- front-end-interview-test/src/CHANGEME.js
+++ front-end-interview-test/src/CHANGEME.js
@@ -6,10 +6,17 @@ class Test extends Component {
 
 state = {
     property: [],
-    buildings: []
+    buildings: [],
+    bedmin:0,
+    bedmax:10,
+    bathmin:0,
+    bathmax:10,
+    buildingtypeselected:'',
+    list: []
+
 }
 
-componentDidMount() {
+   componentDidMount() {
     this.getLocations();
     this.getBuildingTypes();    
   }
@@ -20,7 +27,6 @@ componentDidMount() {
             this.setState({
               property: res.data
             })
-
           )
           .catch(err => console.log(err));
       };
@@ -32,49 +38,59 @@ componentDidMount() {
             this.setState({
               buildings: res.data
             })
-
           )
           .catch(err => console.log(err));
       };
 
 
-      filterList = (event) => {
-        let updatedList = this.getLocations();
-
-        updatedList = updatedList.filter(function(item){
-            return item.search(
-              event.target.value) !== -1;
-          });
+      onChange= (event)=>{
+      this.setState({[event.target.name]: event.target.value}); 
+      }   
 
 
-        this.setState({property: updatedList});
-      };
-
-
+      onChangeBuilding= (event)=>{
+      this.setState({buildingtypeselected: event.target.value});  
+      }
+      
+      
+      filterList = () => {
+        let list=[];
+        for (var i = 0;this.state.property.length>i; i++) {
+               if((this.state.property[i].beds >= this.state.bedmin && this.state.property[i].beds <= this.state.bedmax)&&
+               (this.state.property[i].baths >= this.state.bathmin && this.state.property[i].baths <= this.state.bathmax) && 
+               (this.state.property[i].buildingType.name === this.state.buildingtypeselected))
+               {
+                list.push(this.state.property[i]);
+              }
+         }
+      this.setState({list:list});  
+    };
 
       
     render() {
         return (
             <div className="testContainer">
+
                 <div className="filterContainer">
                     
+                <p>Beds</p>
+                <input type="number" onChange={this.onChange} name="bedmin" placeholder="Min" />
+                <input type="number" onChange={this.onChange} name="bedmax" placeholder="Max" />
+                 <p>Baths</p>
+                <input type="number" onChange={this.onChange} name="bathmin" placeholder="Min" />
+                <input type="number" onChange={this.onChange} name="bathmax" placeholder="Max" />
 
-                <input type="number" placeholder="# of Beds" />
-
-                
-                <input type="number" placeholder="# of Baths" onChange={this.filterList}/>
-
-                <br />
-
+                  
                 <p>Building Type</p>
-                <select>
-                {this.state.buildings.map(buildings => (
-                    <option key={buildings.id} value={buildings.name}>{buildings.name}</option>
+                <select onChange={this.onChangeBuilding} name='buildingtypeselected'>
+                {this.state.buildings.map(building=> (
+                    <option key={building.id} value={building.name}>
+                    {building.name}
+                    </option>
                 ))}
                 </select>
-
-
-                </div>
+                <button onClick={this.filterList}>Submit</button>
+                </div> 
                 <RemineTable properties={this.state.property} />
             </div>
         );
